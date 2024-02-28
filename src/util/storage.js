10295a7a4e31a60ac54cbec8086c8f1e0f0c937b
michael <michael.samiley@gmail.com>
2024-02-27 23:12:37 -0800

    mongo config

--- src/util/storage.js
+++ src/util/storage.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
-
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const db_connectString = process.env.db_connection_string
 
 const StoreData = (data) => {
+    mongoose.connect(db_connectString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
     // console.log("meow")
     console.log(data)
+    mongoose.connection.close()
 }
 
 module.exports =  { StoreData }
